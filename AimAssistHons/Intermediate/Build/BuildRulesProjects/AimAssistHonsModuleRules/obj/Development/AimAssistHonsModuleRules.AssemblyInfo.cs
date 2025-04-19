54914c31968591dd16b25e52dc5ea80d5010e426
2101598 <2101598@uad.ac.uk>
2025-04-19 19:01:39 +0100

    Further Blueprint clean up

--- AimAssistHons/Intermediate/Build/BuildRulesProjects/AimAssistHonsModuleRules/obj/Development/AimAssistHonsModuleRules.AssemblyInfo.cs
+++ AimAssistHons/Intermediate/Build/BuildRulesProjects/AimAssistHonsModuleRules/obj/Development/AimAssistHonsModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AimAssistHonsModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e707c5e5d19a10e7de17bef380235cb5953aaf98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a25a2f0cbf954e93286687ede1a4f53f0912351")]
 [assembly: System.Reflection.AssemblyProductAttribute("AimAssistHonsModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AimAssistHonsModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ AimAssistHons/Intermediate/Build/Win64/x64/AimAssistHonsEditor/DebugGame/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.cpp.old
@@ -0,0 +1,2 @@
+// Compiler: 14.36.32548
+#include "C:/Users/hamil/OneDrive/Documents/GitHub/AimAssitanceHons_KyleH/AimAssistHons/Intermediate/Build/Win64/x64/AimAssistHonsEditor/DebugGame/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h"
